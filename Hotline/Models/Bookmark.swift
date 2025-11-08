2112b181aa654fc9d05770d09aa0cd098cd76bc9
Dustin Mierau <dustin@mierau.me>
2025-11-08 15:37:25 -0800

    Change default bookmark to MacDomain.

--- Hotline/Models/Bookmark.swift
+++ Hotline/Models/Bookmark.swift
@@ -76,7 +76,7 @@ final class Bookmark {
   
   static let DefaultBookmarks: [Bookmark] = [
     Bookmark(type: .server, name: "The Mobius Strip", address: "mobius.trtphotl.com", port: HotlinePorts.DefaultServerPort),
-    Bookmark(type: .server, name: "System 7 Today", address: "hotline.system7today.com", port: HotlinePorts.DefaultServerPort),
+    Bookmark(type: .server, name: "MacDomain", address: "hotline.system7today.com", port: HotlinePorts.DefaultServerPort),
     Bookmark(type: .tracker, name: "Featured Servers", address: "hltracker.com", port: HotlinePorts.DefaultTrackerPort)
   ]
   
